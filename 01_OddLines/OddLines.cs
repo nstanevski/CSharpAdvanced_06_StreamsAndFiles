a524ae9820e14b346f2214187b0dfb60e8671fb2
nstanevski <nikolay.stanevski@gmail.com>
2015-05-20 08:19:33 +0300

    task#1

---
+++ 01_OddLines/OddLines.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+
+class OddLines
+{
+    static void Main()
+    {
+        using (var reader = new StreamReader("../../App.config")){
+            int n = 0;
+            string line;
+            while((line = reader.ReadLine()) != null){
+                if (n % 2 == 1)
+                    Console.WriteLine(line);
+                n++;
+            }
+        }     
+    }
+}
