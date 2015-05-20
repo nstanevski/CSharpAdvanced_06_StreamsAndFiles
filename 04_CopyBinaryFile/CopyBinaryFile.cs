1d883e44b8ff99c4778d5406ef20b1c0db6e42f8
nstanevski <nikolay.stanevski@gmail.com>
2015-05-20 20:41:12 +0300

    task#4

---
+++ 04_CopyBinaryFile/CopyBinaryFile.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+
+class CopyBinaryFile
+{
+    static void Main()
+    {
+        using (FileStream source = new FileStream("../../../03_WordCount/bin/Debug/03_WordCount.exe", FileMode.Open))
+        {
+            using (FileStream dest = new FileStream("../../../03_WordCount/bin/Debug/03_WordCountCopy.exe", FileMode.Create))
+            {
+                byte[] buffer = new byte[4096];
+                int len;
+                while ((len = source.Read(buffer, 0, buffer.Length)) > 0)
+                    dest.Write(buffer, 0, len);
+            }
+        }
+    }
+}
